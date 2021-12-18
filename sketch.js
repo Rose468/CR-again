3586f21e770db76124eb9127e55ce9754818bb50
Rose468 <85027884+Rose468@users.noreply.github.com>
2021-12-18 19:25:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,10 @@ var school;
 var player;
 function preload()
 {
-  carAnimation1=loadAnimation("images/car1.png");
-  carAnimation2=loadAnimation("images/car2.png");
-  playerAnimation=loadAnimation("images/Player-03.png");
-  logAnimation=loadAnimation("images/log2.png");
+  carAnimation1=loadAnimation("car1.png");
+  carAnimation2=loadAnimation("car2.png");
+  playerAnimation=loadAnimation("Player-03.png");
+  logAnimation=loadAnimation("log2.png");
  
 }
 
